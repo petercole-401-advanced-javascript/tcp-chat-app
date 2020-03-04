6a64b2715bee5952a28b07048ab46808ef993427
‘peter-robert-cole’ <petercole3@gmail.com>
2020-03-03 17:05:33 -0800

    time is working, and only prints to user

---
+++ server/__tests__/server.test.js
@@ -0,0 +1,17 @@
+
+const {doWithBuffer, broadcast} = require('../server')
+
+describe('server module', () => {
+  xdescribe('doWithBuffer()', () => {
+    it('properly translates raw data to a trimmed string', () => {
+      const rawData = '<Buffer 7b 22 65 76 65 6e 74 54 79 70 65 22 3a 22 6d 65 73 73 61 67 65 22 2c 22 70 61 79 6c 6f 61 64 22 3a 22 3c 6b 64 3e 3a 20 6b 64 22 7d>';
+      expect(doWithBuffer(rawData)).toEqual('<kd>: kd');
+    });
+  });  
+  xdescribe('broadcast()', () => {
+    it('', () => {
+      
+    });
+  });
+});
+ 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 
-// THIS IS ALL NODE, CLI, Sockerts will be tomorrow
+// THIS IS ALL NODE, CLI, Sockets will be tomorrow
 // Server is gonna listen for two basic event types
-//
 
 const net = require('net')
 const server = net.createServer()
@@ -15,26 +14,37 @@ server.listen(PORT, () => {
 const socketPool = {}
 
 server.on('connection', socket => { // whatever socket connected, make more events specific to that connection
-  const id = `Socket-${Math.random()}` // no unique guarantee, temporary packageless technique
-  socketPool[id] = socket
-  socket.on('data', buffer => doWithBuffer(buffer))
+  const id = `Socket-${Math.random() * 1000}` // no unique guarantee, temporary packageless technique
+  socketPool[id] = socket;
+  socket.on('data', buffer => doWithBuffer(id, buffer))
   socket.on('error', err => console.error('SOCKET ERROR', err))
   socket.on('end', () => delete socketPool[id])
 })
-
 server.on('error', err => console.error('SERVER ERROR', err))
+
 
 // when we get some data,
 
-function doWithBuffer(){
+function doWithBuffer(id, buffer) {
   const message = JSON.parse(buffer.toString().trim())
-  broadcast(message)
+  broadcast(id, message)
 }
 
-function broadcast(message){
-  const payload = JSON.stringify(message)
-  for (const socket in socketPool){
-    socketPool[socket].write(payload)
+function broadcast(id, message) {
+  socketPool[id]
+  console.log(message)
+  console.log('payload.messageType:', message.messageType)
+  if (message.messageType === 'personal') {
+    console.log('UNIQUE PRINT to', id)
+    const payload = JSON.stringify(message)
+    socketPool[id].write(payload)
+  } else {
+    for (const socket in socketPool) {
+      console.log('PRINTED TO EVERYONE')
+      const payload = JSON.stringify(message)
+      socketPool[socket].write(payload)
+    }
   }
 }
 
+module.exports = { doWithBuffer, broadcast };
